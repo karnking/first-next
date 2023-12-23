ce17740696283bef88222b7c2b654254abacbb55
karnking <karankotai9183@gmail.com>
2023-12-23 21:00:25 +0530

    new list

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -12,6 +12,9 @@ const UsersPage = async () => {
                 {users.map(user=> <li key={user.id}>
                     {user.name}
                 </li>)}
+                {users.map(user=> <li key={user.id}>
+                    {user.name}
+                </li>)}
             </ul>
         </>
     )
