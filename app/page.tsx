c50a68220619d06d5ec1edb4e131132d5b00a822
karnking <karankotai9183@gmail.com>
2023-12-25 22:34:54 +0530

    new project

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 import Link from 'next/link'
-import ProductCard from './components/ProductCard'
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello World</h1>
-      <Link href='/users'>Users</Link>
-      <ProductCard />
+    <main className='flex min-h-screen flex-col p-6'>
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52"></div>
     </main>
   )
 }
