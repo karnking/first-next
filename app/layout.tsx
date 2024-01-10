ee08329ccd39a96afbceb89d4b4173988f5be73b
karnking <karankotai9183@gmail.com>
2024-01-10 21:12:27 +0530

    navbar added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,4 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './ui/globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'First Next',
